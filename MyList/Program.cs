547fe60697f3a4b01a3899c4e6cd083702c6291c
gunnurraksoy@gmail.com <gunnurraksoy@gmail.com>
2021-02-04 23:13:23 +0300

    Add project files.

---
+++ MyList/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MyListProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> sehirler = new MyList<string>();
+            sehirler.Add("Ankara");
+            sehirler.Add("İstanbul");
+            sehirler.Add("İzmir");
+            sehirler.Add("Zonguldak");
+            sehirler.Add("Kastamonu");
+            sehirler.Add("Antalya");
+
+            Console.WriteLine(sehirler.Count);
+            
+        }
+
+        
+
+
+        
+    }
+}
